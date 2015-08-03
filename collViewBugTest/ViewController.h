f917033eac7117254891de6c809907ba3c9f5c31
Alexey Saechnikov <a.saechnikov@vironit.com>
2015-08-03 11:55:32 +0300

    Initial Commit

---
+++ collViewBugTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collViewBugTest
+//
+//  Created by Алексей Саечников on 03.08.15.
+//  Copyright (c) 2015 aSaechnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
